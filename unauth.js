d8cd047c9627e9e95083006336ab098d94061cbd
psyfer <praneshnegi@gmail.com>
2019-10-11 19:55:03 +0530

    Add files via upload

---
+++ unauth.js
@@ -0,0 +1,14 @@
+var str = document.getElementsByTagName('div')[0].innerHTML.toString();
+var i = 0;
+document.getElementsByTagName('div')[0].innerHTML = "";
+
+setTimeout(function() {
+    var se = setInterval(function() {
+        i++;
+        document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+        if (i == str.length) {
+            clearInterval(se);
+            document.getElementsByTagName('div')[0].innerHTML = str;
+        }
+    }, 10);
+},0);
